6a3e4d502d46b24aa1a685ffcc652e7b1ab18c21
Siddhant Prasad <75333983+SiddhantStark@users.noreply.github.com>
2024-08-11 21:16:01 +0530

    Update Provider.jsx

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client';
 
 import { SessionProvider } from "next-auth/react";
@@ -10,16 +9,5 @@ const Provider = ({ children, session }) => (
 )
 
 export default Provider;
-=======
-import React from 'react'
 
-const Provider = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default Provider
->>>>>>> cdbe64c3606cd1e6e53fae01dea2d6b871f99369
